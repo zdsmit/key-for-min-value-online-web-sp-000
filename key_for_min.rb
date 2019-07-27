b4cfe90d0387e0d7e860a7cd4ce0ddb1794426e2
Zach  Smith <zdsmith001@gmail.com>
2019-07-27 15:29:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
   name_hash.each do |key, value|
-    if value < min_value
+    if min_key == nil || value < min_value
       min_key = key
       min_value = value
     end
