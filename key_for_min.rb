e0ea69ce9ae56cd391374b99a0ddf02716dede8e
Zach  Smith <zdsmith001@gmail.com>
2019-07-27 15:34:52 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,11 @@
 def key_for_min_value(name_hash)
   min_key = nil
   min_value = nil
+  name_hash.each do |key, value|
+    if min_key == nil || value < min_value
+      min_key = key
+      min_value = value
+    end
+  end
+  min_key
 end
